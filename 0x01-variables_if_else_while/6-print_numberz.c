bc3d0969c4123be5dee8f20dac2f4c8f46b49ffd
AberBenjo <benjelloun857@gmail.com>
2023-07-15 16:10:00 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - main func
+ *Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
