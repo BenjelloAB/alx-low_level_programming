16cfcb2a684587077e59d56720ffb9097d155df3
AberBenjo <benjelloun857@gmail.com>
2023-07-16 14:12:42 -0700

    Streams v2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
+	int len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (1);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), len, stderr);
+	return (1);
 }
