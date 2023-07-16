c8ef3d53765e4700b129414e2e2682eda877be59
AberBenjo <benjelloun857@gmail.com>
2023-07-16 06:37:38 -0700

    Hexadecimalv1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - printong hexa nums in lower case
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
