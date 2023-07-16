0fc4378367cbae64d1fa15ae05d55aa385ca147c
AberBenjo <benjelloun857@gmail.com>
2023-07-16 07:25:13 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - outputting using puts()
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
