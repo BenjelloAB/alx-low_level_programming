edda07cf2d527aab623486c7c4613c9a731df250
AberBenjo <benjelloun857@gmail.com>
2023-07-19 17:30:37 -0700

    times table v2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,14 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			n = i * j;
-			if (n > 9)
+			if (n <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
 				_putchar((n / 10) + 48);
+			}
 			_putchar((n % 10) + 48);
 			if (j != 9)
 				_putchar(',');
