6dab0b2e78675f156b8f7137a090cbf1be1312be
AberBenjo <benjelloun857@gmail.com>
2023-07-19 18:24:28 -0700

    98 Battery Street v3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - print from n to 98
  * @n: the delimiter for the range
@@ -7,34 +8,18 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	if ( n <= 98)
+	if (n >= 98)
 	{
-		for(i = n; i <= 98; i++)
-		{
-			if (i > n)
-				_putchar(' ');
-			if (i > 9)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			if (i < 98)
-				_putchar(' ');
-		}
+		while(n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+
 	}
 	else
 	{
-		for(i = n;i >= 98; i--)
-		{
-			if(i > n)
-				_putchar(' ');
-			if (i > 9)
-				_putchar((i /10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			if( i > 98)
-				_putchar(' ');
-		}
+		while(n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
+		
 }
