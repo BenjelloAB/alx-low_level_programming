33a6919c8e09dcb2cb7ede65832bc4166eaa1287
AberBenjo <benjelloun857@gmail.com>
2023-07-19 19:24:14 -0700

    n times table max 15 v1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * prrint_times_table - prints the n's times table
+ * @n: holds the times table limit
+ * Return: times table of n
+ */
+
+void print_times_table(int n)
+{
+	
+	if (i > 0 && i < 15)
+	int i, j ,res;
+
+	{
+		for(i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++ )
+			{
+				res = i * j;
+				if (res > 99)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10) % 10) + '0');
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+				}
+				if (res > 9 && res < 100)
+				{
+					_putchar((res / 10) + '0');
+				}
+				else if (j != 0 && res < 100)
+				{
+					_putchar(' ');
+				}
+				_putchar((res % 10) + '0');
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+		
+
+}
