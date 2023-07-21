b968bbd9b1ca2cac813fd2d803e51470b8c1c1da
AberBenjo <benjelloun857@gmail.com>
2023-07-21 15:56:50 -0700

    diagonal universe v5

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,9 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	if (n > 0){
+
+	if (n > 0)
+	{
 	for (i = 0; i < n; i++)
 	{
 	for (j = 0; j < i; j++)
@@ -23,7 +25,4 @@ void print_diagonal(int n)
 	{
 	_putchar('\n');
 	}
-	
-
 }
-
