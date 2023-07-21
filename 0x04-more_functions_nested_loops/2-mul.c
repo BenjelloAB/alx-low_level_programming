9b597b4572eb978b57c3a568fa17194209494953
AberBenjo <benjelloun857@gmail.com>
2023-07-21 13:20:33 -0700

    mulv1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: var to cpature first int
+ * @b: var to capture second int
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
