58b9323b83391e87ddf624b880ea1e25f2848d0f
AberBenjo <benjelloun857@gmail.com>
2023-07-21 14:17:04 -0700

    straight line v1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print n * '_' 
+ * @n: var to hold the number of time we print '_'
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
