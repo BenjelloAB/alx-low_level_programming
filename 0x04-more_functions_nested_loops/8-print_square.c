45476689b7a2ca3368fe5edbcb63c3f804f8dd0b
AberBenjo <benjelloun857@gmail.com>
2023-07-21 16:11:26 -0700

    square inch_v2

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,19 @@ void print_square(int size);
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < 5; i++)
 	{
 	for (j = 0; j < 5; j++)
 	{
-	putchar('#');
+	_putchar('#');
 	}
-	putchar('\n');
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
 	}
 }
