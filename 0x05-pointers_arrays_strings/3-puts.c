1d8383f8f3f191e5689f68c01bc37a2f45a52882
AberBenjo <benjelloun857@gmail.com>
2023-07-25 14:30:24 -0700

    puts clone v1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a newline 
+ * @str: pointer that points to a string
+ * Return: void nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
