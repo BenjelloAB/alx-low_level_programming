9c418ef5e5d9d5057058e834a2b0f6187fb3307b
AberBenjo <benjelloun857@gmail.com>
2023-07-25 12:59:49 -0700

    98 Battery v1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the past pointer's value to 98
+ * @n: capture the pointer to the value we want to resest
+ * Return: void nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
