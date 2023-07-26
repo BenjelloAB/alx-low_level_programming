7fa997b026c6ba95759ee8ea28c6a04b3e4cad21
AberBenjo <benjelloun857@gmail.com>
2023-07-25 19:05:22 -0700

    strcpy_clone_v1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string from src to dest
+ * @dest: capture the pointer that should pint to src string
+ * @src: capture the string that should be pointed by dest pointer
+ * Return: pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
