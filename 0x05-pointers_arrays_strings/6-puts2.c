7dac46e2872292217c9cf1ea87203dcb72c6b327
AberBenjo <benjelloun857@gmail.com>
2023-07-25 17:27:04 -0700

    Half_lies_v1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - printf every other character of a string
+ * @str: var to capture the pointer pointing to the string
+ * Return: void nothing
+ */
+
+void puts2(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
