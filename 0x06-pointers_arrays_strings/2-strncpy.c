70d1c706912acf376be8fc99deb81575dc834565
AberBenjo <benjelloun857@gmail.com>
2023-07-28 02:02:06 -0700

    strncpy_v3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 /**
- * _strncpy - dest string  concatenated with n bytes from src string
- * @dest: pointer holding first string to concat with
- * @src: pointer holding the string to concat upon
- * @n: number of bytes to take from src string
- * Return: pointer
- */
+	* _strncpy - dest string  concatenated with n bytes from src string
+	* @dest: pointer holding first string to concat with
+	* @src: pointer holding the string to concat upon
+	* @n: number of bytes to take from src string
+	* Return: pointer
+	*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *str = dest;
+	int i = 0, len = 0;
 
-	int i = 0;
-
-	int k = n;
-
-	int len = 0;
-
-	while (dest[i++])
-	{
+	while (src[i++])
 		len++;
-	}
-	while (n > 0 && *src != '\0')
-	{
-		*dest = *src;
-		src++;
-		dest++;
-		n--;
-	}
-	if (len < n)
-		*dest = '\0';
-	return (str);
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = len; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
