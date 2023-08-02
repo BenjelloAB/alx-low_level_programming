71f88eccfd8ceba746cecb54c199b1cc60dc6895
AberBenjo <benjelloun857@gmail.com>
2023-08-01 18:19:53 -0700

    strspn_v1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - calculates the number of bytes depending on other str
+ * @s: pointer to the initial string
+ * @accept: pointer to the target string
+ * Return: int uns , a number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	int j = 0;
+
+	int count = 1;
+
+	while (accept[i])
+	{
+	while (s[j])
+	{
+		if (accept[i] == s[j])
+		{
+			count++;
+			break;
+		}
+		j++;
+	}
+	j = 0;
+	i++;
+	}
+	return (count);
+}
