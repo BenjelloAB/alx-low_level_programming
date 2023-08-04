d4b4f566a4523a9614ecef4d7231eeee9282388b
AberBenjo <benjelloun857@gmail.com>
2023-08-04 16:51:00 -0700

    nargos_v1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - returns the number of arguments passed into it
+ * @argv: the list of arguments passed in
+ * @argc: the number of arguments passed in
+ * Return: int
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
