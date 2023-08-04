20c93d836c726812891283fdce48d0f54134629f
AberBenjo <benjelloun857@gmail.com>
2023-08-04 16:39:59 -0700

    print_prog_name_v2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
  * main - function that prints the program name
- * @argc: number of arguments passed 
+ * @argc: number of arguments passed
  * @argv: list of argument passed
  * Return: int
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
