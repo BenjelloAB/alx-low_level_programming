9c982753fe9ef9ee215b2c3b2e002c75d2a2ea33
AberBenjo <benjelloun857@gmail.com>
2023-08-04 16:37:33 -0700

    print_prog_name_v1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main - function that prints the program name
+ * @argc: number of arguments passed 
+ * @argv: list of argument passed
+ * Return: int
+ */
+
+int main(int argc,char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
