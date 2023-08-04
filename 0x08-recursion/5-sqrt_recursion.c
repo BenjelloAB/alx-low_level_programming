36eeb2912956625bb994d46963d715b52b2f03f6
AberBenjo <benjelloun857@gmail.com>
2023-08-03 17:40:45 -0700

    sqrt_v1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * f_sqr - checks if a num has a natural square
+ * @f: integer
+ * @s: integer
+ * Return: int
+ */
+int f_sqr(int f, int s)
+{
+
+	if ((s * s) == f)
+	{
+		return (s);
+	}
+	if ((s * s) > f)
+		return (-1);
+	return f_sqr(f, s + 1);
+
+}
+/**
+ * _sqrt_recursion - checks if a num has a natural square
+ * @n: integer to calcualte its square
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return(0);
+	if (n == 1)
+		return (1);
+	if (n < 1)
+		return (-1);
+	return f_sqr(n, 1);
+}
