24c5aa31bf1b9370cb56ca7d6161d88a9b087508
AberBenjo <benjelloun857@gmail.com>
2023-08-03 17:42:24 -0700

    sqrt_v2

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,7 +15,7 @@ int f_sqr(int f, int s)
 	}
 	if ((s * s) > f)
 		return (-1);
-	return f_sqr(f, s + 1);
+	return (f_sqr(f, s + 1));
 
 }
 /**
@@ -26,10 +26,10 @@ int f_sqr(int f, int s)
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
-		return(0);
+		return (0);
 	if (n == 1)
 		return (1);
 	if (n < 1)
 		return (-1);
-	return f_sqr(n, 1);
+	return (f_sqr(n, 1));
 }
