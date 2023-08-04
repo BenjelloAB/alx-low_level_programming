ebf6740b73945b3a0cc75dfae89484e493c9a804
AberBenjo <benjelloun857@gmail.com>
2023-08-03 18:24:12 -0700

    Prime_rec_v1

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * isPrimeRec - checks if a number is a prime number
+ * @n: integer
+ * @div: integer
+ * Return: int
+ */
+int isPrimeRec(int n, int div)
+{
+	if (div == 1)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (isPrimeRec(n, n - 1));
+}
+/**
+ * is_prime_number - checks if a number is prime number
+ * @n: integer
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (isPrimeRec(n, n - 1));
+}
