0d3e64704eab98940613d38db6249e2693789e7a
AberBenjo <benjelloun857@gmail.com>
2023-08-03 19:24:38 -0700

    palidrome_v2

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,32 @@
 #include "main.h"
 
 /**
- * rec_palindrome - checks if a string is palindrome
+ * rec_palindrome_2 - checks if a string is palindrome
  * @s: pointer to the string
  * @i: integer
  * @len: length of the string
  * Return: int
  */
-int rec_palindrome(char *s, int i, int len)
+int rec_palindrome_2(char *s, int i, int len)
 {
 	if (i >= len / 2)
 		return (1);
 	if (s[i] != s[len - 1 - i])
 		return (0);
-	return (rec_palindrome(s, i + 1, len));
+	return (rec_palindrome_2(s, i + 1, len));
+}
+/**
+ * rec_palindrome - checks if a stirng is palindrome or not
+ * @s: integer
+ * @i: integer
+ * @len: integer
+ * Return: int
+ */
+int rec_palindrome(char *s, int i, int len)
+{
+	if (s[i] == '\0')
+		return (rec_palindrome2(s, 0, len));
+	return (rec_palindrome(s, i + 1, len + 1));
 }
 /**
  * is_palindrome - checks if a string is palindrome
@@ -23,12 +36,8 @@ int rec_palindrome(char *s, int i, int len)
 int is_palindrome(char *s)
 {
 	int i = 0;
+
 	int len = 0;
 
-	while (s[i])
-	{
-		len++;
-		i++;
-	}
-	return (rec_palindrome(s, 0, len));
+	return (rec_palindrome(s, i, len));
 }
