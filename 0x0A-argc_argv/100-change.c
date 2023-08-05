2ae2f86a1ae5ab185982b56d18aacf03332f39df
AberBenjo <benjelloun857@gmail.com>
2023-08-04 19:16:03 -0700

    change_v1

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+	* main - prints min number of coimns to make change for an amount of money
+	* @argv: list of arguments
+	* @argc: number of arguments
+	*Return: int
+	*/
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	int number;
+
+	int j = 0;
+
+	if (argc < 2)
+	{
+	puts("Error");
+	return (1);
+	}
+	number = atoi(argv[1]);
+	while (1 == 1)
+	{
+	if (number == 0)
+		break;
+	if (number >= 25)
+	{
+	number -= 2
+	count++;
+	continue;
+	}
+	if (number >= 10)
+	{
+	number -= 10;
+	count++;
+	continue;
+	}
+	if (number >= 5)
+	{
+	number -= 5;
+	count++;
+	continue;
+	}
+	if (number >= 1)
+	{
+	number -= 1;
+	count++;
+	continue;
+	}
+	}
+	printf("%d\n", count);
+
+	return (0);
+}
