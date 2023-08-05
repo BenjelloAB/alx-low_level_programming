703e84e7c2dedd653192f97f9aa73803a6a08a66
AberBenjo <benjelloun857@gmail.com>
2023-08-04 17:12:24 -0700

    argument_arith_v1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return the result of multi of two numbers
+ * @argv: list of arguments
+ * @argc: number of arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int p = 1;
+
+	int number;
+
+	if (argc >= 4 || argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			number = atoi(argv[i]);
+			p *= number;
+		}
+		printf("%d\n", p);
+	}
+	return (0);
+}
