6bf18c064c9c6d79ba1d66af30a932fa9c275d78
AberBenjo <benjelloun857@gmail.com>
2023-08-07 10:54:07 -0700

    women_w_wings_v1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - a function to copy a string in a  new bloc of memory
+ * @str: pointer to the string to copy
+ * Return: pointer to the copy
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	int len = 0;
+
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		i++;
+		len++;
+	}
+	p = (char *)malloc(len * sizeof(char));
+	i = 0;
+	if (p == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		p[i] = str[i];
+		i++;
+	}
+	return (p);
+
+}
