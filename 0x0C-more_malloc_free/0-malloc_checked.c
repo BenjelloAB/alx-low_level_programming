efb1689f2ee473853c94edf341ce3549b81274ec
AberBenjo <benjelloun857@gmail.com>
2023-08-10 10:10:18 -0700

    Trust me v2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * malloc_checked - check if the size to allocate in mem is available
+ * @b: holds the size of the memory to allocate
+ * Return: void
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
-	return p;
+	return (p);
 }
