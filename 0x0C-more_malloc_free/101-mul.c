e97ba2a3380ddfde52cc56c8a00e967ee0ddbdb3
AberBenjo <benjelloun857@gmail.com>
2023-08-10 17:29:25 -0700

    Possible_v2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
+ * printError - prints an error
+ * Return: void
  */
-void printError()
+void printError(void)
 {
 	_putchar('E');
 	_putchar('r');
@@ -17,9 +17,9 @@ void printError()
 }
 
 /**
- *
- *
- *
+ * checkIfNum - checks if a string contains only digits with ASCII code
+ * @s: string to check
+ * Return: int
  */
 int checkIfNum(const char *s)
 {
@@ -27,29 +27,28 @@ int checkIfNum(const char *s)
 
 	while (s[i])
 	{
-		if(!(s[i] >= 48 && s[i] <= 57))
+		if `(!(s[i] >= 48 && s[i] <= 57))
 			printError();
 		i++;
 	}
 	return (atoi((const char *)s));
 }
 /**
- *
- *
- *
- *
+ * main - main function
+ * @argv: arguments
+ * @argc: number of argumnets
+ * Return: int
  */
 int main(int argc, const char *argv[])
 {
 	int num1, num2, num3;
+
 	if (argc > 3)
 		printError();
 
 	num1 = checkIfNum(argv[1]);
 	num2 = checkIfNum(argv[2]);
 	num3 = num1 * num2;
-	printf("%d\n",num3);
+	printf("%d\n", num3);
 	return (0);
-	
-
 }
