1d5e09c0ea17c6ca256da358ea05557733daeee2
AberBenjo <benjelloun857@gmail.com>
2023-08-14 14:28:51 -0700

    FIle_name_v2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,9 @@
 
 /**
  * main - program to print the name of the file it was compiled from
- * @argc: number of args passed
- * @argv: List of args passed
  * Return: int
  */
-int main(int argc, char const *argv[])
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
