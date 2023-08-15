8223968b1c9abe8eae28956014c861369f789232
AberBenjo <benjelloun857@gmail.com>
2023-08-15 14:25:33 -0700

    Poppy_v2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines the dog structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+
 struct dog
 {
 	char *name;
