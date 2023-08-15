a5c162dee5335514ea3a0bebf53269d29febd1cb
AberBenjo <benjelloun857@gmail.com>
2023-08-15 14:31:53 -0700

    init_w_pointers_v1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize the struct dog variable
+ * @d: pointer to thedog structure object 
+ * @name: pointer to the name of the dog's string
+ * @age: integer holding the dog's age
+ * @owner: pointer to the owner string
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
