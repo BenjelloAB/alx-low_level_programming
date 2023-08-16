816e12117d180557a0d84cb01dd8076448e7d57e
AberBenjo <benjelloun857@gmail.com>
2023-08-15 17:18:03 -0700

    doggy_v1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * copy_str - copies a sting into another palce in memory to be safe
+ * @copy: pointer to the bloc in Heap to copy to
+ * @str: string to copy
+ * Return: void
+ */
+void copy_str(char *copy, char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+}
+/**
+ * len_str - calculates the length of a string
+ * @str: pointer to the string
+ * Return: int -> lenght of the string including 0 byte
+ */
+
+int len_str(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
+ * new_dog - creates a new dog of type dog_t
+ * @age: integer holding the age of the dog
+ * @name: pointer to the name string
+ * @owner: pointer to the owner string
+ * Return: dog_t pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+
+	doggy = (dog_t *)malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+	doggy->name = (char *)malloc(sizeof(char) * len_str(name));
+	if (doggy->name == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
+	copy_str(doggy->name, name);
+	doggy->owner = (char *)malloc(sizeof(char) * len_str(owner));
+	if (doggy->owner == NULL)
+	{
+		free(doggy->owner);
+		free(doggy);
+		return (NULL);
+	}
+	copy_str(doggy->owner, owner);
+	doggy->age = age;
+	return (doggy);
+}
