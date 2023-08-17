34737f2971a243daebcc7af1b77a7b05bab22529
AberBenjo <benjelloun857@gmail.com>
2023-08-17 12:17:35 -0700

    One_woman_v1

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints args seprated with the seperator string
+ * @separator: pointer to the string to use as sep between args
+ * @n: number of args passed
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(args, char *) == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", va_arg(args, char *));
+		}
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
