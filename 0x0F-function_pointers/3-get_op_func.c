9ab1ec22d36d23519f4b15b55682cd073bdfd077
AberBenjo <benjelloun857@gmail.com>
2023-08-16 19:29:51 -0700

    calc_v1

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+	* get_op_func - selects the correct funtion to perform
+	* @s: pointer to the op string literal
+	* Return: pointer to a function that returns int
+	*/
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	i = 0;
+	while (i < 5)
+	{
+		if (*s == *(ops[i].op))
+			return ops[i].f;
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+
+/**
+ * print_error - print the error code
+ * @code: code of exit
+ * Return: void
+ */
+void print_error(int code)
+{
+	printf("Error\n");
+	exit(code);
+}
+/**
+ * main - testing the calculator
+ * @argv: array of pointers to each arg
+ * @argc: number of args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		print_error(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+		print_error(99);
+	printf("%d\n", op(a, b));
+	return (0);
+}
