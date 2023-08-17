fb2fa90417aaf9f6372f7e888d88cedb31d27c97
AberBenjo <benjelloun857@gmail.com>
2023-08-17 12:09:41 -0700

    join_func_v2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * print_numbers - prints args seprated with the seperator string
  * @separator: pointer to the string to use as sep between args
@@ -20,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator != NULL && i != n - 1)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
