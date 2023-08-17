c2acffc4e8061f3419ff5c9768e27e85572040fc
AberBenjo <benjelloun857@gmail.com>
2023-08-17 12:12:03 -0700

    join_func_v3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	if (n == 0)
-		return;
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
