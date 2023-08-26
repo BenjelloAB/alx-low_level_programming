5004a7629c8e8510d1e7d5a46f6ccd09eb1a7a8e
AberBenjo <benjelloun857@gmail.com>
2023-08-25 18:48:03 -0700

    constructor_funcs_v1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * func1 - runs before main func
+ * Return: nothing
+ */
+void func1(void)__attribute__((constructor));
+void func1(void)
+{
+printf("You're beat! and yet, you must allow,"
+		"\nI bore my house upon my back!\n");
+}
