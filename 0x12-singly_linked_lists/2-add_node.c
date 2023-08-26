afdb0013b76c87875adb558160c7ca60a55e114d
AberBenjo <benjelloun857@gmail.com>
2023-08-25 18:11:24 -0700

    lists_t2_v1

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - add a node at beg of a list
+ * @head: pinter to ponter of list_t staruct type
+ * @str: pointer to string
+ * Return: list_t
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *t = NULL;
+
+	unsigned int len1 = 0, i = 0;
+
+	char *copy;
+
+	t = (list_t *)malloc(sizeof(list_t));
+	if (t == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		i++;
+		len1++;
+	}
+	len1--;
+	copy = strdup(str);
+	t->str = copy;
+	t->len = len1;
+	t->next = *head;
+	*head = t;
+	return (*head);
+}
