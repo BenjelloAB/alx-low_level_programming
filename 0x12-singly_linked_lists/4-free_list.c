ead75131a087377cab7e44ed17465988fa1433d2
AberBenjo <benjelloun857@gmail.com>
2023-08-25 18:39:28 -0700

    lists_t4_v1

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function to free the 
+ * @head: pointer to the lsit
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
+
