41602b354cfe051bf2e2c11d91bb0f5faeb564d6
AberBenjo <benjelloun857@gmail.com>
2023-08-29 18:00:03 -0700

    t6_v2

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,10 +9,13 @@ int pop_listint(listint_t **head)
 {
 	listint_t *next;
 
+	int n;
+
 	if (*head == NULL)
 		return (0);
 	next = (*head)->next;
+	n = (*head)->n;
 	free(*head);
 	*head = next;
-	return (1);
+	return (n);
 }
