38e5b196938a4cfc2ea22f82c4fcfa14b219d2a9
AberBenjo <benjelloun857@gmail.com>
2023-08-29 18:22:49 -0700

    task8_v1

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_liatint - returns the sum of the integers inside all teh nodes
+ * @head: pointer to the lsit
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
