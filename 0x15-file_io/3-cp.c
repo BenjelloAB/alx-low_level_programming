12ada652b51e702edc7103bbd60c80a645e2d3fe
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-10-04 16:09:57 -0700

    task4_v10

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/stat.h>
-
+#include <fcntl.h>
 
 /**
  * check_buffer - check if the buffer is NULL
- * @b: pointer to the buffer
  * @file: file name
  * Return: void
  */
-void check_buffer(char *b, const char *file)
+char *check_buffer(const char *file)
 {
+	char *b = malloc(sizeof(char) * 1024);
+
 	if (b == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
+	return (b);
 }
 /**
  * closer - function to close open files using their files descriptor
@@ -39,24 +41,48 @@ void closer(int fd1, int fd2)
 	c2 = close(fd2);
 	if (c2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't colse fd %d\n", fd2);
 		exit(100);
 	}
 }
 /**
- * handle_write - handles -1 write value situation
- * @buffer: the buffer to read the data into
- * @fd1: the file descriptor of file_from
- * @fd2: the file descriptor of file_to
- * @file_name: thePOSIX standard error file name
+ * handle_errors - handles -1 values situations in sys calls
+ * @b: the buffer to read the data into
+ * @f1: the file descriptor of file_from
+ * @f2: the file descriptor of file_to
+ * @fi: thePOSIX standard error file name
+ * @e: string to specify the err type
  * Return: void
  */
-void handle_write(char *buffer, int fd1, int fd2, const char *file_name)
+void handle_errors(char *b, int f1, int f2, const char *fi, const char *e)
 {
-	closer(fd1, fd2);
-	free(buffer);
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
-	exit(99);
+	if (strcmp(e, "open_f1") == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fi);
+		free(b);
+		exit(98);
+	}
+	if (strcmp(e, "write") == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fi);
+		free(b);
+		closer(f1, f2);
+		exit(99);
+	}
+	if (strcmp(e, "open") == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fi);
+		free(b);
+		closer(f1, f2);
+		exit(99);
+	}
+	if (strcmp(e, "read") == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fi);
+		free(b);
+		closer(f1, f2);
+		exit(98);
+	}
 }
 /**
  * check_argc - checks the number of arguments passed
@@ -79,41 +105,42 @@ void check_argc(int argc)
  */
 int main(int argc, const char *argv[])
 {
-	int fd1, fd2, w, bytesRead, currOffSet = 0;
+	int fd1, fd2, w, bytesRead;
 	char *buffer = NULL;
 
-	buffer = malloc(sizeof(char) * 1024);
+	buffer = check_buffer(argv[2]);
 	check_argc(argc);
-	check_buffer(buffer, argv[2]);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		handle_errors(buffer, fd1, 1, argv[1], "open_f1");
 	umask(0);
-	fd2 = open(argv[2], O_CREAT | O_EXCL | O_WRONLY | O_APPEND, 0664);
-	if (fd2 < 0)
+	fd2 = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (fd2 == -1)
 	{
 		if (errno == EEXIST)
-			fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_APPEND);
-
+		{
+			fd2 = open(argv[2], O_WRONLY | O_TRUNC);
+			if (fd2 == -1)
+				handle_errors(buffer, fd1, fd2, argv[2], "open");
+		}
+		else
+			handle_errors(buffer, fd1, fd2, argv[2], "open");
 	}
 	bytesRead = read(fd1, buffer, 1024);
-	do {
-		if (bytesRead == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+	if (bytesRead == -1)
+		handle_errors(buffer, fd1, fd2, argv[1], "read");
+	while (bytesRead > 0)
+	{
 		w = write(fd2, buffer, bytesRead);
 		if (w == -1)
-			handle_write(buffer, fd1, fd2, argv[2]);
-		if (lseek(fd1, currOffSet, SEEK_SET) == -1)
-			handle_write(buffer, fd1, fd2, argv[2]);
-		currOffSet += bytesRead;
+			handle_errors(buffer, fd1, fd2, argv[2], "write");
 		bytesRead = read(fd1, buffer, 1024);
-	} while (bytesRead > 0);
+		if (bytesRead == -1)
+			handle_errors(buffer, fd1, fd2, argv[1], "read");
+		fd2 = open(argv[2], O_APPEND | O_WRONLY);
+		if (fd2 == -1)
+			handle_errors(buffer, fd1, fd2, argv[2], "open");
+	}
 	closer(fd1, fd2);
 	free(buffer);
 	return (0);
