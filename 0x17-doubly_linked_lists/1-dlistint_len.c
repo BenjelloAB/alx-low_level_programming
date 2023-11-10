b59085018d6d2d6db7078cfc2dfa3da16c599bfe
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-10 08:34:17 -0800

    task2_v1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the length of a dlist
+ * @h: pointer to the header of the list
+ * Return: sizeof of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t s = 0;
+	const dlistint_t temp = h;
+
+	while (temp)
+	{
+		s++;
+		temp = temp->next;
+	}
+	return (s);
+}
