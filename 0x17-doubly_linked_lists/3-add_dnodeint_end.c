50ccd82d9dd6393230ce754442c517de49898a9f
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-10 10:14:22 -0800

    task4_v1.1

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp = NULL;
 
 	if (new_node == NULL)
-		return (NULL);	
+		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new_node);
 	}
 	temp = *head;
-	while(temp->next)
+	while (temp->next)
 		temp = temp->next;
 	temp->next = new_node;
 	new_node->prev = temp;
