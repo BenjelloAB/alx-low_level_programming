e748738fa06857251e6bfdf61de4e5ffa0955720
Abderrahmane <benjelloun857@gmail.com>
2023-12-06 21:18:01 +0100

    task0_v1.2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,18 +4,20 @@
  * hash_table_create - create a hash table
  * @size : the size of the hash table
  * Return: a pointer to the hash table
- *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 	ht->size = size;
-	ht->array = (hash_node_t **)calloc(ht->size, sizeof(hash_node_t *));
+	ht->array = (hash_node_t **)malloc(ht->size * sizeof(hash_node_t *));
 	if (!ht->array)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 	return (ht);
 }
